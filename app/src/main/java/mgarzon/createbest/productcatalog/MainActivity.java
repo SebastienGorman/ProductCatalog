b4eb6544f8edf910bd4526e66e5646f32ae38607
Sébastien Gorman <sgorm@uottawa.ca>
2018-11-06 18:03:52 -0500

    first change

--- app/src/main/java/mgarzon/createbest/productcatalog/MainActivity.java
+++ app/src/main/java/mgarzon/createbest/productcatalog/MainActivity.java
@@ -25,6 +25,8 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
 import java.util.List;
 
+
+
 public class MainActivity extends AppCompatActivity {
 
     EditText editTextName;
